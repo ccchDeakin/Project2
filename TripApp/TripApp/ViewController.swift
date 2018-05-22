5693882c1513ff89be5cbe31c39e0dca35148b33
CHUN CHEUNG CHOW <ccch@10-150-44-233.dhcp-m.deakin.edu.au>
2018-05-22 14:19:37 +1000

    1st check in

---
+++ TripApp/TripApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TripApp
+//
+//  Created by CHUN CHEUNG CHOW on 22/5/18.
+//  Copyright © 2018 CHUN CHEUNG CHOW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
